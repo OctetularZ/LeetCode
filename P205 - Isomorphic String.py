35ff6d101433052bbafea2e4d99fb20a9419232f
OctetularZ <taiabayomi15@gmail.com>
2024-12-26 16:47:00 +0000

    Added isomorphic string optimal solution

--- P205 - Isomorphic String.py
+++ P205 - Isomorphic String.py
@@ -14,20 +14,18 @@ def isIsomorphic(s: str, t: str) -> bool:
 
 # Optimal solution
 
-# indexS = [0] * 200  # Stores index of characters in string s
-# indexT = [0] * 200  # Stores index of characters in string t
+# def isIsomorphic(self, s: str, t: str) -> bool:
+#     char_index_s = {}
+#     char_index_t = {}
 #
-# length = len(s)  # Get the length of both strings
+#     for i in range(len(s)):
+#         if s[i] not in char_index_s:
+#             char_index_s[s[i]] = i
 #
-# if length != len(t):  # If the lengths of the two strings are different, they can't be isomorphic
-#     return False
+#         if t[i] not in char_index_t:
+#             char_index_t[t[i]] = i
 #
-# for i in range(length):  # Iterate through each character of the strings
-#     if indexS[ord(s[i])] != indexT[ord(t[
-#                                            i])]:  # Check if the index of the current character in string s is different from the index of the corresponding character in string t
-#         return False  # If different, strings are not isomorphic
+#         if char_index_s[s[i]] != char_index_t[t[i]]:
+#             return False
 #
-#     indexS[ord(s[i])] = i + 1  # updating position of current character
-#     indexT[ord(t[i])] = i + 1
-#
-# return True  # If the loop completes without returning false, strings are isomorphic
+#     return True
