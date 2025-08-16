0dcb24f4cf0ebf7dd07c0c6611f93a65c967ad17
OctetularZ <taiabayomi15@gmail.com>
2025-08-16 02:14:02 +0100

    Added P338 - Counting Bits

---
+++ P338 - Counting Bits.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = []
+        for i in range(n + 1):
+            ans.append(bin(i).count("1"))
+
+        return ans
