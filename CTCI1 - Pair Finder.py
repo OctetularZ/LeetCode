bdac47d7780eb3f9c4fa137834c768bfa411fc79
OctetularZ <taiabayomi15@gmail.com>
2025-08-18 17:38:09 +0100

    Added Cracking the Coding Interview (random question drawn from book. CH4)

---
+++ CTCI1 - Pair Finder.py
@@ -0,0 +1,41 @@
+# Find pairs [a, b] in array where the difference between [a, b] == k
+
+def binary_search(arr, target):
+  left, right = 0, len(arr) - 1
+
+  while left <= right:
+    mid = (left + right) // 2
+
+    if arr[mid] == target:
+      return True
+    
+    if arr[mid] < target:
+      left = mid + 1
+    else:
+      right = mid - 1
+  
+  return False
+
+def find_pairs(arr, k):
+  pairs = []
+  
+  for i in range(len(arr)):
+    print(arr[i])
+    m = binary_search(arr, arr[i] - k)
+    n = binary_search(arr, k + arr[i])
+
+    if m == True:
+      pairs.append([arr[i], arr[i] - k])
+    if n == True:
+      pairs.append([arr[i], k + arr[i]])
+  
+  return pairs
+
+
+# Loop through array once
+# For each element in the array, binary search for a - k or b - k
+# If found, it is a pair, add to pairs
+
+#Test
+arr = [1, 7, 5, 9, 2, 12, 3]
+print(find_pairs(arr, 2))
