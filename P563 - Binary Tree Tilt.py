7e753e4b183b61b99218c3e321a8b5e3f4eded77
OctetularZ <taiabayomi15@gmail.com>
2025-08-28 19:26:36 +0100

    Added 563 - Binary Tree Tilt

---
+++ P563 - Binary Tree Tilt.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        total = 0
+
+        def dfs(node):
+            nonlocal total
+            if node is None:
+                return 0
+            
+            if node.left is None and node.right is None:
+                return node.val
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+            
+            total += abs(left - right)
+
+            return node.val + left + right
+        
+        dfs(root)
+
+        return total
+    
+
+# From the left and right subtree, I need the total sum of nodes then I can find the absolute difference of both
+# Base case - If there are no nodes to traverse to/ if node is none.
