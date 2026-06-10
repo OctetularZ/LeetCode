ffb94b0bf46babf19c433e14687ad1fec2e57b59
OctetularZ <taiabayomi15@gmail.com>
2026-06-10 13:25:19 +0100

    Updated P1 - Two Sum

--- P1 - Two Sum (Easy).py
+++ P1 - Two Sum (Easy).py
@@ -1,27 +1,14 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        lst_range = len(nums)
-        iterations = 1
-        for index_one in range(0, lst_range):
-            for index_two in range(iterations, lst_range):
-                first_number = nums[index_one]
-                second_number = nums[index_two]
-                total = first_number + second_number
-                if total == target:
-                    if index_two == index_one:
-                        for index_two_recheck in range(index_two, lst_range):
-                            if index_two_recheck == second_number:
-                                return [index_one, index_two_recheck]
-                            else:
-                                pass
-                    else:
-                        return [index_one, index_two]
-                else:
-                    pass
-            iterations += 1
+from typing import List
 
 
-# Nums (array of integers)
-# Target (target integer)
-# One solution from an array
-# Return indices (index)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_arr = {}
+
+        for i in range(len(nums)):
+            hash_arr[nums[i]] = i
+
+        for i in range(len(nums)):
+            find = target - nums[i]
+            if find in hash_arr and hash_arr[find] != i:
+                return [i, hash_arr[find]]
