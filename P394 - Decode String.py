04255ad2d673433d381a77b67133682aa2a9d391
OctetularZ <taiabayomi15@gmail.com>
2026-06-20 20:37:37 +0100

    Updated P394 - Decode String

--- P394 - Decode String.py
+++ P394 - Decode String.py
@@ -1,34 +1,38 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
-        current_number = 0
-        current_string = ""
+        str_stack = []
+        int_stack = []
+        current_str = ""
+        current_numb = ""
 
-        for char in s:
-            if char == "[": # If start of new sequence:
-                stack.append(current_string) # Add current string to stack
-                stack.append(current_number) # Add current number to stack
-                current_string = "" # Reset both
-                current_number = 0
-            elif char == "]": # If end of sequence:
-                num = stack.pop() # Get number from stack
-                prev_string = stack.pop() # Get previous string from stack
-                current_string = prev_string + (current_string * num) # Multiply current string by number and add string in stack to start
-            elif char.isdigit():
-                current_number = (current_number * 10) + int(char) # If digit, multiply current digit by 10 then add new digit to it
-            else:
-                current_string += char # If letter, add to end of current_string
+        for i in range(len(s)):
+            if s[i].isalpha():
+                current_str += s[i]
+            elif s[i].isnumeric():
+                current_numb += s[i]
+            elif s[i] == '[':
+                str_stack.append(current_str)
+                int_stack.append(int(current_numb))
+                current_str = ""
+                current_numb = ""
+            elif s[i] == ']':
+                temp = current_str
+                current_str = str_stack.pop()
+                number = int_stack.pop()
+                current_str += temp * number
 
-        return current_string
+        return current_str
+
+# [a]
+# [2]
+# bbb
 
 
-
-# Store current string, start as ""
-# Intialise stack, start as []
-# Store current number, start as 0
-
-# For each character in decoded string -
-# If char = [, add current string to stack, add current number to stack, current number = 0, current string = ""
-# If char = ], get current number, get current string (both from stack), multiply then add to start of current string (outside of stack - "total" string essentially)
-# If char = number, current number = current number * 10 + new number
-# If char = letter, current string = current string + character
+# Stack - Only store letters and numbers
+# Iterate though characters in s
+# Global variable to keep track of result string
+# Add each character to stack until close bracket is reached
+# When close bracket is reached, add string to start of global variable
+# When a number is reached, multiply global variable by number
+# When end of s is reached, add any remaining letters to end of res
+# Another variable to store current string we are building so we can add to stack
